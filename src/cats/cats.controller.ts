3e7267cfcb59b6b251bbb2b5b4e56f7016cdcb11
Hanelle <hanella0911@gmail.com>
2022-12-11 12:37:44 +0900

    Dependency injection

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+import { Cat } from 'interfaces/cat.interface';
+import { CatsService } from './cats.service';
+
+
+@Controller('cats')
+export class CatsController {
+    //Dependency Injection
+    constructor(private catsService: CatsService) { }
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto)
+    }
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    //Route parameters
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        console.log(id)
+        return `This action retruns a #${id} cat`;
+    }
+
+    @Get('docs')
+    @Redirect('http://nestjs.com', 302)
+    getDocs(@Query('version') version) {
+        if (version && version === '5') {
+            return { url: 'http://docs.nestjs.com/v5/' };
+        }
+    }
+}
+
